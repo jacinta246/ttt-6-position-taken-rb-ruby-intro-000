3d33448a83e21ea71111df7352056f9c50e749e9
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-11 17:38:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 def position_taken? (board, index)
- return board [index]==false
- if board [index] == "X" || board [index] "O"
-   #puts "Sorry we are out of stock!"
- #else
-   #puts "Thanks for your order!"
+ return board[index]==false
+ if board[index] == "X" || board[index] == "O"
+   return true
  end
 end
